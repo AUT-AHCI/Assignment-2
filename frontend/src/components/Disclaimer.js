11c866dc0606ee5bd27a579d6bfbc74b886d90ed
coolo-droid <70293739+coolo-droid@users.noreply.github.com>
2021-05-18 12:45:42 +1200

    Changed date

--- frontend/src/components/Disclaimer.js
+++ frontend/src/components/Disclaimer.js
@@ -21,7 +21,7 @@ const Disclaimer = () => {
 								<input type="checkbox" id="term1" name="term1" /><br />
 							</td>
 							<td>
-								<label for="term1">I have read and understood the <a href="" target="_blank">information</a> provided about this research project in the Information Sheet dated dd mmmm yyyy.</label><br />
+								<label for="term1">I have read and understood the <a href="" target="_blank">information</a> provided about this research project in the Information Sheet dated 18/05/2021.</label><br />
 							</td>
 						</tr>
 						<tr>
@@ -93,27 +93,7 @@ const Disclaimer = () => {
 							</td>
 						</tr>
 					</table>
-					
-					
-					
-					
 				</form>
-				{/* <input type="checkbox" id="accept"> Accept</input> */}
-					{/* <p>
-						The purpose of this research project is to explore the useability of single sign on (SSO) compared to signing up with a traditional form. We will then get you to take a survey at the end to gather your feedback on the useability of both sign-up methods.
-						This research is being conducted by Ashely Thew, Aaron Gilbert, Travis Hun and Robert Ankersmit at the Auckland University of Technology (AUT).
-						You are invited to participate in this research project because you have the capability to use a variety of sign-up methods, such as SSO and the traditional form sign up.
-						Your participation in this research project is voluntary and you can choose not to participate. If you decide to participate in this research project, you may withdraw at any time. If you decide not to participate or withdraw from the research project prematurely, you will not be penalized in any way.
-						The survey questions will be about how you found using the different sign-up methods and whether one method was more usable for you when compared with the other sign-up method.
-						The procedure involves signing in with either a SSO or a traditional form sign up, this will take approximately 5 to 10 minutes or less in some cases. Your responses will be confidential and will not collect your IP address, name,	
-						The results of this study will be used for scholarly purposes only and may be shared with AUT representatives.
-						If you have any questions in regards to the research please contact.
-						This research has been approved by Dr Robert Wellington from AUT.
-						By clicking on ‘agree’ button below this indicates the following:
-						-	You have read the above information.
-						-	You are voluntarily agreeing to participate.
-						If you do not wish to participate, please select the ‘disagree’ option.
-					</p> */}
 				</div>
 			</Container>
 		</div>
