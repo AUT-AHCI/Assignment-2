d9cb2cce31e03fd9460aefe259f5bdd333b4a173
Travish <83637066+TravisPDC@users.noreply.github.com>
2021-05-18 18:31:00 +1200

    About us
    Changes

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -5,27 +5,7 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 const AboutUs = () => {
 	
 	return (
-<<<<<<< Updated upstream
-		<div>
-			<Container className="d-flex flex-column justify-content-center align-items-center h-100 my-5">
-				<h2>About US</h2>
-				<div className="d-flex flex-column justify-content-center align-items-center h-100 my-5">
-					<h3>Name</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, omnis sequi earum, aspernatur itaque non iure deserunt, exercitationem cum voluptates id. Dolore maiores molestiae vero voluptatem nesciunt! Quis, quam perspiciatis?</p>
-				</div>
-				<div className="d-flex flex-column justify-content-center align-items-center h-100 my-5">
-					<h3>Name</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut et doloremque quidem vero enim unde ipsum reprehenderit ea. Odio, quidem cumque! Alias porro facere aperiam doloremque provident amet modi excepturi?</p>
-				</div>
-				<div className="d-flex flex-column justify-content-center align-items-center h-100 my-5"
-				>
-					<h3>Name</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, modi voluptatibus labore obcaecati molestias at, omnis, enim sunt minima dolorum hic! Voluptate nemo consequuntur sequi ex architecto aut in quae.</p></div>
-				<div className="d-flex flex-column justify-content-center align-items-center h-100 my-5"
-				>
-					<h3>Name</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores sed, illo tempore dignissimos numquam molestiae quos labore nesciunt, quis repellat corrupti ut iure perspiciatis eos accusamus doloribus! Aperiam, repudiandae omnis.</p>
-=======
+		
 		<div class="wrapper">
 			<h1><b>About US</b></h1>
 			<Container className="team">
