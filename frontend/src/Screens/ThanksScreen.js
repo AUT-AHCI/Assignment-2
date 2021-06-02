6020d890d93a01d178df3466433388c03d8896ca
AshleyThew <ashl.e.thew@gmail.com>
2021-06-02 16:26:24 +1200

    Thanks screen
    Thanks screen

---
+++ frontend/src/Screens/ThanksScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Thanks from '../components/Thanks';
+
+const TestScreen = ({ history }) => {
+	return (
+		<>
+			<Thanks></Thanks>
+		</>
+	);
+};
+
+export default TestScreen;
--- frontend/src/components/Findings.js
+++ frontend/src/components/Findings.js
@@ -1,9 +1,8 @@
-import React, { useEffect, } from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import axios from 'axios';
 
 const Findings = () => {
-	
 	const containerStyle = {
 		background: 'rgba(0, 0, 0, 0.6)',
 		backgroundPosition: 'center',
@@ -12,41 +11,52 @@ const Findings = () => {
 	};
 
 	return (
-        <div style={containerStyle}>
-            <Container className="d-flex flex-column justify-content-center align-items-center w-50">
-            
-                <br/><br/><br/>
-                <h1>Findings</h1>
-                <br/>
-                <p>Over the course of our testing phase, we were able to retrieve x results from participants.</p>
+		<div style={containerStyle}>
+			<Container className="d-flex flex-column justify-content-center align-items-center w-50">
+				<br />
+				<br />
+				<br />
+				<h1>Findings</h1>
+				<br />
+				<p>
+					Over the course of our testing phase, we were able to retrieve x results from
+					participants.
+				</p>
 
-                <br/><br/><br/>
-                <h1>How The Testing Went</h1>
-                <br/>
-                <p>it hasn't</p>
+				<br />
+				<br />
+				<br />
+				<h1>How The Testing Went</h1>
+				<br />
+				<p>Researching</p>
 
-                <br/><br/><br/>
-                <h1>Results</h1>
-                <br/>
-                <p>we need some of these probably</p>
+				<br />
+				<br />
+				<br />
+				<h1>Results</h1>
+				<br />
+				<p>Researching</p>
 
-                <br/><br/><br/>
-                <h1>Data Analysis</h1>
-                <br/>
-                <p>looks like the cheese is melted</p>
+				<br />
+				<br />
+				<br />
+				<h1>Data Analysis</h1>
+				<br />
+				<p>Researching</p>
 
-                <br/><br/><br/>
-                <h1>HCI Discoveries</h1>
-                <br/>
-                <p>wow</p>
+				<br />
+				<br />
+				<br />
+				<h1>HCI Discoveries</h1>
+				<br />
+				<p>Researching</p>
 
-                <br/><br/><br/>
-            </Container>
-
-        </div>
-
-    );
-
+				<br />
+				<br />
+				<br />
+			</Container>
+		</div>
+	);
 };
 
 export default Findings;
